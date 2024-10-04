59d6c593e51f1f727d530ebd11afe79d213a2e30
Andrew Naumann <35020306+AndresNaumann@users.noreply.github.com>
2024-10-04 12:13:54 -0600

    navigation change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -19,7 +19,7 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/chat">
               Chat
             </Nav.Link>
             <Nav.Link as={Link} to="/about">
